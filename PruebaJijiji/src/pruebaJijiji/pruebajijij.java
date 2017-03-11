47b4d50ca496bbb185fe258287f42c5775ff0e13
PcCom <PcCom@192.168.1.128>
2017-03-11 17:09:22 +0100

    Intento fallido del billon

--- PruebaJijiji/src/pruebaJijiji/pruebajijij.java
+++ PruebaJijiji/src/pruebaJijiji/pruebajijij.java
@@ -10,34 +10,34 @@ public class pruebajijij {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double tiempoInicio = System.currentTimeMillis();
-		
+		System.out.println("Este es el primer for");
 		LinkedList<Persona> ll = new LinkedList<>();
-		int contador = 0;
+		double contador = 0;
 		ll.addLast(new Persona());
-		for(; contador < 10_000_000;)
+		/*for(; contador < 1_000_000_000d;)
 		{
 
 			ll.add(new Persona());
 			if(contador%2==0)ll.removeLast();
 			contador++;
-		}
+		}*/
 		double tiempoFin = System.currentTimeMillis();
 		System.out.println((tiempoFin-tiempoInicio)/1000+" segundos");
 		
 		
 		
 			tiempoInicio = System.currentTimeMillis();
-		
+			System.out.println("Este es el segundo for");
 		ArrayList<Persona> ll2 = new ArrayList<>();
 		contador = 0;
 		ll2.add(new Persona());
-		for(; contador < 10_000_000;)
+		/*for(; contador < 1_000_000_000d;)
 		{
 
 			ll2.add(new Persona());
 			if(contador%2==0)ll2.remove(0);
 			contador++;
-		}
+		}*/
 		tiempoFin = System.currentTimeMillis();
 		System.out.println((tiempoFin-tiempoInicio)/1000+" segundos");
 		
@@ -45,11 +45,11 @@ public class pruebajijij {
 		
 		
 		tiempoInicio = System.currentTimeMillis();
-		
+		System.out.println("Este es el tercer for");
 		Vector<Persona> ll3 = new Vector<>();
 		 contador = 0;
 		ll3.add(new Persona());
-		for(; contador < 10_000_000;)
+		for(; contador < 1_000_000_000d;)
 		{
 			
 			ll3.add(new Persona());
